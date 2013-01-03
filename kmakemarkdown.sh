acaf3d8ef759ac3a6ce147627c2efa4e5fd8803b
TaeDong Kim <fly1004@gmail.com>
2013-01-03 12:01:39 +0900

    chapter1의 po 파일을 완료함

---
+++ kmakemarkdown.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+
+
+
+po2txt -t en/01-introduction/01-chapter1.markdown po/01_ko.po ko/01-introduction/01-chapter1.markdown
+po2txt -t en/02-git-basics/01-chapter2.markdown po/02_ko.po ko/02-git-basics/01-chapter2.markdown
+po2txt -t en/03-git-branching/01-chapter3.markdown po/03_ko.po ko/03-git-branching/01-chapter3.markdown
+po2txt -t en/04-git-server/01-chapter4.markdown po/04_ko.po ko/04-git-server/01-chapter4.markdown
+po2txt -t en/05-distributed-git/01-chapter5.markdown po/05_ko.po ko/05-distributed-git/01-chapter5.markdown
+po2txt -t en/06-git-tools/01-chapter6.markdown po/06_ko.po ko/06-git-tools/01-chapter6.markdown
+po2txt -t en/07-customizing-git/01-chapter7.markdown po/07_ko.po ko/07-customizing-git/01-chapter7.markdown
+po2txt -t en/08-git-and-other-scms/01-chapter8.markdown po/08_ko.po ko/08-git-and-other-scms/01-chapter8.markdown
+po2txt -t en/09-git-internals/01-chapter9.markdown po/09_ko.po ko/09-git-internals/01-chapter9.markdown
