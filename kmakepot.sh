9a1ee034837f838f4c762011bf10876cd67d0a4b
TaeDong Kim <fly1004@gmail.com>
2013-01-03 12:45:17 +0900

    각 chapter별로 pot/po 파일을 가지고 있던 것을 하나의 파일로 통합함.

--- kmakepot.sh
+++ kmakepot.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
 
-txt2po -P en/01-introduction/01-chapter1.markdown > po/01.pot
-txt2po -P en/02-git-basics/01-chapter2.markdown > po/02.pot
-txt2po -P en/03-git-branching/01-chapter3.markdown > po/03.pot
-txt2po -P en/04-git-server/01-chapter4.markdown > po/04.pot
-txt2po -P en/05-distributed-git/01-chapter5.markdown > po/05.pot
-txt2po -P en/06-git-tools/01-chapter6.markdown > po/06.pot
-txt2po -P en/07-customizing-git/01-chapter7.markdown > po/07.pot
-txt2po -P en/08-git-and-other-scms/01-chapter8.markdown > po/08.pot
-txt2po -P en/09-git-internals/01-chapter9.markdown > po/09.pot
+txt2po -P en/01-introduction/01-chapter1.markdown > ktemp_01.pot
+txt2po -P en/02-git-basics/01-chapter2.markdown > ktemp_02.pot
+txt2po -P en/03-git-branching/01-chapter3.markdown > ktemp_03.pot
+txt2po -P en/04-git-server/01-chapter4.markdown > ktemp_04.pot
+txt2po -P en/05-distributed-git/01-chapter5.markdown > ktemp_05.pot
+txt2po -P en/06-git-tools/01-chapter6.markdown > ktemp_06.pot
+txt2po -P en/07-customizing-git/01-chapter7.markdown > ktemp_07.pot
+txt2po -P en/08-git-and-other-scms/01-chapter8.markdown > ktemp_08.pot
+txt2po -P en/09-git-internals/01-chapter9.markdown > ktemp_09.pot
+
+
+msgcat ktemp_*.pot > po/progit.pot
+rm -fr ktemp_*.pot
--- po/update_po.sh
+++ po/update_po.sh
@@ -1,13 +1,4 @@
 #!/bin/sh
 
 
-pot2po -t 01_ko.po 01.pot -o 01_ko.po
-pot2po -t 02_ko.po 02.pot -o 02_ko.po
-pot2po -t 03_ko.po 03.pot -o 03_ko.po
-pot2po -t 04_ko.po 04.pot -o 04_ko.po
-pot2po -t 05_ko.po 05.pot -o 05_ko.po
-pot2po -t 06_ko.po 06.pot -o 06_ko.po
-pot2po -t 07_ko.po 07.pot -o 07_ko.po
-pot2po -t 08_ko.po 08.pot -o 08_ko.po
-pot2po -t 09_ko.po 09.pot -o 09_ko.po
-
+pot2po -t progit_ko.po progit.pot -o progit_ko.po
